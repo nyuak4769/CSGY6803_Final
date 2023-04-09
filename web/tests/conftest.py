8c335246789a4ce8dd6416f81008df66ba54d001
Austin D. Krauza <krauza.austin@gmail.com>
2023-04-08 23:04:48 -0400

    Add Integration Tests

---
+++ web/tests/conftest.py
@@ -0,0 +1,5 @@
+class ValueStorage:
+    dummy_secret = {"description": "testing secret", "policyname": "My Policy", "value": "abc123"}
+    dummy_secret_id = None
+    num_of_secrets = -1
+    num_of_events = -1
